ded710e3b32c691a9080d13edccc1e6ca7296d58
Marc G <mac_gere@yahoo.com>
2023-09-12 21:16:36 -0500

    now im just having auth issues

---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyB--e1MNY_5RvDgOE4tWQDadaaEv6jGBeo",
+  authDomain: "marc-chat-68b3c.firebaseapp.com",
+  projectId: "marc-chat-68b3c",
+  storageBucket: "marc-chat-68b3c.appspot.com",
+  messagingSenderId: "104422644642",
+  appId: "1:104422644642:web:2d94ec929277993be5f0fb",
+  measurementId: "G-H64GZM6MJ1"
+};
